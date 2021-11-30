202f1e10cda11cdc0a8dfe06b2db87f811c52525
Yannick B <yannickbiheul@gmail.com>
2021-11-30 12:33:46 +0100

    Doctrine et entités

--- src/Controller/ProjetsController.php
+++ src/Controller/ProjetsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Project;
+use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +11,25 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProjetsController extends AbstractController
 {
     /**
-     * @Route("/projets", name="projets")
+     * @Route("/projects", name="projects")
      */
-    public function index(): Response
+    public function index(ProjectRepository $projectRepo): Response
     {
+        $projects = $projectRepo->findAll();
+        
         return $this->render('projets/index.html.twig', [
             'controller_name' => 'ProjetsController',
+            'projects' => $projects,
+        ]);
+    }
+
+    /**
+     * @Route("/projects/{id}", name="projects_show")
+     */
+    public function show(Project $project) 
+    {
+        return $this->render('projets/show.html.twig', [
+            'project' => $project,
         ]);
     }
 }
